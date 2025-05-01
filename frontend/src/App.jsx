d3fa3dd2185a64bb0870c9a01f43a2bebcca2daf
saurabhhh777 <jeeaspirantwallah2022@gmail.com>
2025-05-01 21:36:22 +0530

    typo fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -63,11 +63,11 @@ export default function TempMailPage() {
   }
 
   return (
-    <div className="p-6 font-sans max-w-6xl mx-auto">
-      <h1 className="text-3xl font-bold mb-4 text-orange-500">
-        <span className="text-yellow-600">@</span>TEMP<span className="text-blue-600">MAIL</span>.in
+    <div className="p-6 font-sans w-screen h-screen mx-auto bg-[#08141c] text-white">
+      <h1 className="text-3xl font-bold mb-4 text-[#445460]">
+        <span className="text-[#445460]">@</span>TEMP<span className="text-blue-600">MAIL</span>.in
       </h1>
-      <div className="bg-gray-100 p-4 rounded-lg shadow mb-4">
+      <div className=" p-4 rounded-lg shadow mb-4 bg-[#121f2a]">
         <label className="font-medium">Your temporary email address</label>
         <div className="flex items-center mt-2">
           <input
@@ -83,26 +83,26 @@ export default function TempMailPage() {
             <Copy size={16} /> Copy
           </button>
         </div>
-        <div className="flex gap-4 items-center mt-2 text-sm text-gray-600">
-          <span>Autorefresh in <span className="font-semibold text-black border-2 pl-1.5 pr-1.5  rounded-full">{autorefresh}</span></span>
+        <div className="flex gap-4 items-center mt-2 text-sm text-white">
+          <span>Autorefresh in <span className="font-semibold border-2 pl-1.5 pr-1.5  rounded-full">{autorefresh}</span></span>
           <button className="hover:underline" onClick={handleRefresh}>🔄 Refresh</button>
           <button className="hover:underline" onClick={handleChangeEmail}>✏️ Change</button>
         </div>
       </div>
 
-      <div className="grid grid-cols-3 gap-4">
+      <div className="grid grid-cols-3 gap-4 bg-[#121f2a] shadow">
         <div className="col-span-1 border rounded shadow">
           <div className="border-b p-3 font-semibold">Inbox</div>
-          <div className="p-3 hover:bg-gray-200 cursor-pointer" onClick={() => setSelectedMail(dummyEmail)}>
+          <div className="p-3 hover:bg-gray-600 hover:rounded-2xl cursor-pointer" onClick={() => setSelectedMail(dummyEmail)}>
             <div className="font-bold">Hello</div>
             <div className="text-sm text-blue-600">{dummyEmail.subject}</div>
-            <div className="text-xs text-gray-500 truncate">{dummyEmail.body}</div>
+            <div className="text-xs truncate">{dummyEmail.body}</div>
           </div>
         </div>
 
         <div className="col-span-2 border rounded shadow p-4">
           <div className="font-semibold text-lg mb-2">Welcome</div>
-          <pre className="whitespace-pre-wrap text-sm text-gray-700">{selectedMail.body}</pre>
+          <pre className="whitespace-pre-wrap text-sm ">{selectedMail.body}</pre>
         </div>
       </div>
     </div>
